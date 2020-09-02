27f6ad509cbfd8570acb20b39f3096797affc0b8
Melissa Mercado <melissa.a.merc@gmail.com>
2020-09-02 17:00:58 -0400

    navbar stuff

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,16 @@ const About = () => {
   return (
     <Jumbotron>
       <h1>Welcome!</h1>
-      {/* <img src = "/assets/" className "card-img" alt= "Melis"> */}
+      {/* <img src = "/assets/lawnpic.jpg" className "card-img" alt= "Melis"> */}
       <p>
         Hello everyone! My name is Melissa Mercado. Here are my links to my
         resources.
       </p>
       <p>
-        <Button variant="primary">Learn more</Button>
+        <Button variant="primary" href="">
+          Linkedin
+        </Button>
+        <Button variant="primary">Github</Button>
       </p>
     </Jumbotron>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,11 +7,21 @@ const Header = () => {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Melissa Mercado</Navbar.Brand>
         <Nav className="mr-auto">
-      <Nav.Link className= "navLink" href="#Portfolio">Portfolio</Nav.Link>
-      <Nav.Link className= "navLink" href="#Contact">Contact</Nav.Link>
-      <Nav.Link className= "navLink" href="#About">About</Nav.Link>
+      <Nav.Link className= "navLink" href="/Portfolio">Portfolio</Nav.Link>
+      <Nav.Link className= "navLink" href="/Contact">Contact</Nav.Link>
+      <Nav.Link to="/About" {location.pathname === "/about"}>About</Nav.Link>
       </Navbar>
     </div>
   );
 };
+
 export default Navbar;
+
+{/* 
+<Link
+        to="/"
+        className={location.pathname === "/" ? "navbar-brand" : "nav-link"}
+      >
+        Home
+        onClick
+      </Link> */}
