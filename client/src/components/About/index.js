76b17a8cd5290ecaacb674473bd0c0a8a1cf8f94
Melissa Mercado <melissa.a.merc@gmail.com>
2020-09-02 20:54:42 -0400

    added readme and gif

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Jumbotron>
       <h1>Welcome!</h1>
-      {/* <img src = "/assets/lawnpic.jpg" className "card-img" alt= "Melis"> */}
+      <img src={lawnpic.jpg} className="card-img" alt="Melis" />
       <p>Hey everybody! Take a look around my page to explore more.</p>
       <p>
         <Button variant="primary" href="">
