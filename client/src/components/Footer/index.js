c7ec0cf67bb8e081a7b66401dbebcbd097312892
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-04 19:11:12 -0400

    added css for footer folder

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="container">
+    <div className="container" id="footer">
       <Navbar bg="light" fixed="bottom" variant="light">
         <Navbar.Brand href="#home" text-align="center">
           Melissa Mercado
