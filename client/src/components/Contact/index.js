f356989f11ccd3d17e011e96837f3eb1ccc46839
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-16 15:01:14 -0400

    implemented favicon

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -10,9 +10,9 @@ const Contact = () => {
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          {/*           <Form.Text className="text-muted">
+            
+          </Form.Text> */}
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
